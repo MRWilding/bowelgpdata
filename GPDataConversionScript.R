877bab0ec524a3ec627a00a3bbe17c8e7ef89c7b
Michael Wilding <Michael.Wilding@phe.gov.uk>
2021-11-29 17:16:09 +0000

    Updates to code to extract more information from text fields.
    First attempt at aggregation and testing the throughput

--- GPDataConversionScript.R
+++ GPDataConversionScript.R
@@ -3,10 +3,10 @@ groundhogDay <- "2021-10-01"
 
 library(groundhog)
 
-groundhog.library(c("dplyr", "tidyr"), groundhogDay)
+groundhog.library(c("dplyr", "tidyr", "testthat"), groundhogDay)
 
 # Set column names for output from file
-ColumnNames <- c("CommissioningOrganisation", "Organisation", "OrganisationCode", "60to69_eligible-on-last-day", 
+ColumnNames <- c("Organisation", "OrganisationCode", "60to69_eligible-on-last-day", 
                  "60to69_invited-to-screening-previous-12-months", "60to69_screened-within-6-months-of-invite", 
                  "60to69_screened-within-previous-30-months", "60to69_Uptake", "60to69_2.5-year-coverage", "60to74_eligible-on-last-day", 
                  "60to74_invited-to-screening-previous-12-months", "60to74_screened-within-6-months-of-invite", 
@@ -19,24 +19,46 @@ path <- "W:\\DataAndInfo_NDT\\NationalDataTeam_Restricted\\OpenExeter\\Bowel\\GP
 test <- read.csv(path, skip = 3, na.strings = c("NA",""))
 
 test2 <- test %>%
+  mutate(RegionType = names(test[1])) %>%
+  rename(RegionName = names(test[1])) %>%
   mutate(CommissioningOrganisation = case_when( # Adds a column of names based on the PCT, CCG or STP the data contains
     endsWith(test$Organisation, "PCT") ~ Organisation,
     endsWith(test$Organisation, "CCG") ~ Organisation,
     endsWith(test$Organisation, "STP") ~ Organisation
   )) %>%
-  select(1, 17, everything()) %>% #reorders the data frame to move the new column to the organisation columns
-  fill(names(test2[c(1,2)]), .direction = "down") %>% # fill the data for the region and commissioning organisation down 
-  filter(!is.na(Organisation)) %>% # remove the rows where there is no Organisation (Removes the blank region rows)
-  filter(CommissioningOrganisation != Organisation) %>% #Remove the rows for the commissioning organisation (can aggregate up from GP data)
-  select(-17) #drop the empty row at the end
+  mutate(CommissioningOrganisationType = case_when( # Adds a column type of organisation based on the end of the name
+    endsWith(test$Organisation, "PCT") ~ "PCT",
+    endsWith(test$Organisation, "CCG") ~ "CCG",
+    endsWith(test$Organisation, "STP") ~ "STP"
+  )) %>%
+  mutate(CommissioningOrganisationCode = ifelse(CommissioningOrganisation == Organisation, Organisation.Code, NA)) %>% # Adds the codes for the organisations
+  select(RegionName, RegionType, CommissioningOrganisation, CommissioningOrganisationCode, CommissioningOrganisationType, everything()) %>% #reorders the data frame to move the new column to the organisation columns
+  fill(RegionName, CommissioningOrganisation, CommissioningOrganisationCode, CommissioningOrganisationType, .direction = "down") %>% # fill the data for the region and commissioning organisation down 
+  filter(!is.na(Organisation)) # remove the rows where there is no Organisation (Removes the blank region rows)
+  
+checkAggregationRaw <- test2 %>%
+  filter(CommissioningOrganisation == Organisation) %>%
+  select(-Organisation, -Organisation.Code)
 
-ColNamesAll <- c(names(test2[1]), ColumnNames) #create a list of column names, keeping the first column name
+test3 <- test2 %>% filter(CommissioningOrganisation != Organisation) %>% #Remove the rows for the commissioning organisation (can aggregate up from GP data)
+  select(-ncol(test2)) #drop the empty row at the end
 
-names(test2) <- ColNamesAll # rename the columns based on the list
+checkAggregationCalc <- test3 %>%
+  group_by(RegionName, RegionType, CommissioningOrganisation, CommissioningOrganisationCode, CommissioningOrganisationType) %>%
+  summarise_at(vars(No..of.eligible.people.on.last.day.of.review.period:X2.5.year.coverage...1), sum, na.rm = T) %>%
+  group_by(.drop = "all")
+
+expect_equivalent(checkAggregationRaw, checkAggregationCalc)
+
+
+
+ColNamesAll <- c("RegionName", "RegionType", "CommissioningOrganisation", "CommissioningOrganisationCode", "CommissioningOrganisationType", ColumnNames) #create a list of column name
+
+names(test3) <- ColNamesAll # rename the columns based on the list
 
 # This section flips the data to long format, splitting the ages into a separate column from the column names, and then flips the values back out
 # to the numerical columns. Effectively it reduces the columns by adding the age data to a separate column
-test3 <- test2 %>%
+test4 <- test3 %>%
   pivot_longer(cols = c(5:16), names_to = c("Age", "Category"), names_sep = "_") %>%
   pivot_wider(names_from = Category)
 
