09835c804c9c30d03b5ae107b5237d8e24d0928f
Michael Wilding <Michael.Wilding@phe.gov.uk>
2021-12-10 15:50:12 +0000

    Updates to PCT assignment using CSV file not internal data
    additional error steps to catch mismatches between filtered and aggregated data and the raw data for each PCT
    
    Update pivots to account for wider data

--- GPDataConversionScript.R
+++ GPDataConversionScript.R
@@ -18,6 +18,8 @@ path <- "W:\\DataAndInfo_NDT\\NationalDataTeam_Restricted\\OpenExeter\\Bowel\\GP
 #reads data into the environment for the path, skipping the first 3 rows of the CSV to get column headings - converting blank cells to NA
 dataRaw <- read.csv(path, skip = 3, na.strings = c("NA",""))
 
+PCTData <- read.csv("Primary_Care_Organisations_(April_2011)_Names_and_Codes_in_England_.csv")
+
 data <- dataRaw %>%
   mutate(RegionType = names(test[1])) %>% # Sets the RegionType column to be the name of the first column
   rename(RegionName = names(test[1])) %>% # Renames the first column
@@ -36,23 +38,51 @@ data <- dataRaw %>%
   fill(RegionName, CommissioningOrganisation, CommissioningOrganisationCode, CommissioningOrganisationType, .direction = "down") %>% # fill the data for the region and commissioning organisation down 
   filter(!is.na(Organisation)) # remove the rows where there is no Organisation (Removes the blank region rows)
 
-# Extracts rows where the Commissioning organisation matches the GP practice column  
-checkAggregationRaw <- test2 %>%
-  filter(CommissioningOrganisation == Organisation) %>%
-  select(-Organisation, -Organisation.Code)
+# Using PCT code from file
 
-data <- dataFiltered %>% filter(CommissioningOrganisation != Organisation) %>% #Remove the rows for the commissioning organisation (can aggregate up from GP data)
+dataPCT <- dataRaw %>%
+  mutate(RegionType = names(test[1])) %>% # Sets the RegionType column to be the name of the first column
+  rename(RegionName = names(test[1])) %>% # Renames the first column
+  left_join(PCTData, by= c("Organisation.Code" = "PCO11CDO")) %>%
+  rename(CommissioningOrganisation = PCO11NM) %>% # rename the column to fit the code
+  mutate(CommissioningOrganisationCode = case_when(
+    !is.na(CommissioningOrganisation) ~ Organisation.Code # Add Code data where it has been match
+    ),
+    CommissioningOrganisationType = "PCT") %>% # Add organisation type column manually (to be automated in future)
+  select(c(1, 17, 19, 21:22, 2:16)) %>% # reorders the data frame to move the new columns to the organisation columns and drop unnecessary columns
+  fill(RegionName, CommissioningOrganisation, CommissioningOrganisationCode, CommissioningOrganisationType, .direction = "down") %>% # fill the data for the region and commissioning organisation down 
+  filter(!is.na(Organisation)) %>% # remove the rows where there is no Organisation (Removes the blank region rows)
+  rename(GPPractice = Organisation)
+
+# Extracts rows where the Commissioning organisation matches the GP practice column  
+checkAggregationRaw <- dataPCT %>%
+  filter(CommissioningOrganisationCode == Organisation.Code) %>%
+  select(-Organisation, -Organisation.Code, - X)
+
+dataFiltered <- dataPCT %>% filter(CommissioningOrganisationCode != Organisation.Code) %>% #Remove the rows where the commissioning organisation codes match the GP practice name (can aggregate up from GP data)
   select(-ncol(test2)) #drop the empty row at the end
 
 #Calculate commissioning organisation data from GP practice data
 checkAggregationCalc <- dataFiltered %>%
   group_by(RegionName, RegionType, CommissioningOrganisation, CommissioningOrganisationCode, CommissioningOrganisationType) %>%
-  summarise_at(vars(No..of.eligible.people.on.last.day.of.review.period:X2.5.year.coverage...1), sum, na.rm = T) %>%
-  group_by(.drop = "all")
+  summarise_at(vars(No..of.eligible.people.on.last.day.of.review.period:X2.5.year.coverage...1), sum, na.rm = T) %>% # adds all numerical values within the groups
+  group_by(.drop = "all") %>%
+  # calculates the uptake and coverage and formats to the same 1 d.p. as the raw data
+  mutate(Uptake.. = as.numeric(format(No..of.people.screened.within.6.months.of.invitation / No..of.people.invited.for.screening.in.previous.12.months * 100, nsmall = 1, digits = 1)), 
+         X2.5.year.coverage.. = as.numeric(format(No..of.people.screened.in.previous.30.months / No..of.eligible.people.on.last.day.of.review.period * 100, nsmall = 1, digits = 1)), 
+         Uptake...1 = as.numeric(format(No..of.people.screened.within.6.months.of.invitation.1 / No..of.people.invited.for.screening.in.previous.12.months.1 * 100, nsmall = 1, digits = 1)),
+         X2.5.year.coverage...1 = as.numeric(format(No..of.people.screened.in.previous.30.months.1 / No..of.eligible.people.on.last.day.of.review.period.1 * 100, nsmall = 1, digits = 1)))
 
 expect_equivalent(checkAggregationRaw, checkAggregationCalc) # checks that the extracted commissioning organisation data matches to the calculated data from the GP practices
 
+errors <- setdiff(checkAggregationCalc, checkAggregationRaw)
+errors2 <- setdiff(checkAggregationRaw, checkAggregationCalc)
 
+errors <- bind_rows(errors,errors2)
+
+if(nrow(errors)>0) {
+  stop("Check PCT data is aggregated properly")
+}
 
 ColNamesAll <- c("RegionName", "RegionType", "CommissioningOrganisation", "CommissioningOrganisationCode", "CommissioningOrganisationType", ColumnNames) #create a list of column name
 
@@ -61,7 +91,7 @@ names(dataFiltered) <- ColNamesAll # rename the columns based on the list
 # This section flips the data to long format, splitting the ages into a separate column from the column names, and then flips the values back out
 # to the numerical columns. Effectively it reduces the columns by adding the age data to a separate column
 dataLong <- dataFiltered %>%
-  pivot_longer(cols = c(5:16), names_to = c("Age", "Category"), names_sep = "_") %>%
+  pivot_longer(cols = c(8:19), names_to = c("Age", "Category"), names_sep = "_") %>%
   pivot_wider(names_from = Category)
 
 # Saves the data as a CSV
